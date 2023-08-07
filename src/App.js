1c31e131862ee1519746f86de7a5f9319391b462
Chrisbadill <8185694+Chrisbadill@users.noreply.github.com>
2023-08-07 00:45:20 -0500

    se agrego el componente Card

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
+import Card from "./components/Card";
 
 function App() {
   const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
 
   console.log(formulario);
   return (
-    <form>
-      <Input
-        label="Nombre"
-        name="name"
-        placeholder="Nombre"
-        value={formulario.name}
-        onChange={handleChange}
-      />
-      <Input
-        label="Apellido"
-        name="lastname"
-        placeholder="Apellido"
-        value={formulario.lastname}
-        onChange={handleChange}
-      />
-    </form>
+    <Card>
+      <form>
+        <Input
+          label="Nombre"
+          name="name"
+          placeholder="Nombre"
+          value={formulario.name}
+          onChange={handleChange}
+        />
+        <Input
+          label="Apellido"
+          name="lastname"
+          placeholder="Apellido"
+          value={formulario.lastname}
+          onChange={handleChange}
+        />
+      </form>
+    </Card>
   );
 }
 
