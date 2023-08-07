ea6d5edc7f33757729cbdee2c1a32f5f78fb4320
Chrisbadill <8185694+Chrisbadill@users.noreply.github.com>
2023-08-07 00:54:17 -0500

    se agrego un div con un padding de 20 px de estilo

--- src/App.js
+++ src/App.js
@@ -10,22 +10,24 @@ function App() {
   return (
     <Container>
       <Card>
-        <form>
-          <Input
-            label="Nombre"
-            name="name"
-            placeholder="Nombre"
-            value={formulario.name}
-            onChange={handleChange}
-          />
-          <Input
-            label="Apellido"
-            name="lastname"
-            placeholder="Apellido"
-            value={formulario.lastname}
-            onChange={handleChange}
-          />
-        </form>
+        <div style={{ padding: 20 }}>
+          <form>
+            <Input
+              label="Nombre"
+              name="name"
+              placeholder="Nombre"
+              value={formulario.name}
+              onChange={handleChange}
+            />
+            <Input
+              label="Apellido"
+              name="lastname"
+              placeholder="Apellido"
+              value={formulario.lastname}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
       </Card>
     </Container>
   );
