43c22e8781aacb184f334396f6cd9b5cff83ef26
Chrisbadill <8185694+Chrisbadill@users.noreply.github.com>
2023-08-07 04:14:12 -0500

    se optimizo el codigo para evitar errores y fallas de rendimiento, seaprando el form y cambiandolo a un componente llamado userForm

--- src/App.js
+++ src/App.js
@@ -1,54 +1,22 @@
 import { useState } from "react";
-import useFormulario from "./hooks/useFormulario";
-import Input from "./components/Input";
 import Card from "./components/Card";
 import Container from "./components/Container";
-import Button from "./components/Button";
+import UserForm from "./components/UserForm";
 
 function App() {
   const [usuarios, setUsuarios] = useState([]);
-  const [formulario, handleChange, reset] = useFormulario({
-    name: "",
-    email: "",
-    lastname: "",
-  });
 
-  const submit = (e) => {
-    e.preventDefault();
-    setUsuarios([...usuarios, formulario]);
-    reset();
+  const submit = (usuario) => {
+    setUsuarios([...usuarios, usuario]);
   };
 
-  console.log(formulario, usuarios);
+  console.log(usuarios);
   return (
     <div style={{ marginTop: "15%" }}>
       <Container>
         <Card>
           <div style={{ padding: 20 }}>
-            <form onSubmit={submit}>
-              <Input
-                label="Nombre"
-                name="name"
-                placeholder="Nombre"
-                value={formulario.name}
-                onChange={handleChange}
-              />
-              <Input
-                label="Apellido"
-                name="lastname"
-                placeholder="Apellido"
-                value={formulario.lastname}
-                onChange={handleChange}
-              />
-              <Input
-                label="Correo"
-                name="email"
-                placeholder="Correo"
-                value={formulario.email}
-                onChange={handleChange}
-              />
-              <Button>Enviar</Button>
-            </form>
+            <UserForm submit={submit} />
           </div>
         </Card>
         <Card>
