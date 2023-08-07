cdef13bc4de29f58fcec931d76f9e65709ba68bc
Chrisbadill <8185694+Chrisbadill@users.noreply.github.com>
2023-08-07 00:52:10 -0500

    se agrego el componente Container

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 import useFormulario from "./hooks/useFormulario";
 import Input from "./components/Input";
 import Card from "./components/Card";
+import Container from "./components/Container";
 
 function App() {
   const [formulario, handleChange] = useFormulario({ name: "", lastname: "" });
 
   console.log(formulario);
   return (
-    <Card>
-      <form>
-        <Input
-          label="Nombre"
-          name="name"
-          placeholder="Nombre"
-          value={formulario.name}
-          onChange={handleChange}
-        />
-        <Input
-          label="Apellido"
-          name="lastname"
-          placeholder="Apellido"
-          value={formulario.lastname}
-          onChange={handleChange}
-        />
-      </form>
-    </Card>
+    <Container>
+      <Card>
+        <form>
+          <Input
+            label="Nombre"
+            name="name"
+            placeholder="Nombre"
+            value={formulario.name}
+            onChange={handleChange}
+          />
+          <Input
+            label="Apellido"
+            name="lastname"
+            placeholder="Apellido"
+            value={formulario.lastname}
+            onChange={handleChange}
+          />
+        </form>
+      </Card>
+    </Container>
   );
 }
 
